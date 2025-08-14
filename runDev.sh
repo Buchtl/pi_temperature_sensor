08c2ae7386860ec912d752c1227b2936dd208f2d
Chris <schauer@inf-schauer.de>
2025-08-14 18:27:21 +0200

    refactor

--- runDev.sh
+++ runDev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m src.plot_temperature --root-dir ./test_device --sensor-filename w1_slave
+python3 -m src.plot_temperature --root-dir ./test_device --sensor-filename w1_slave --period 1
--- src/plot_temperature.py
+++ src/plot_temperature.py
@@ -20,10 +20,18 @@ if __name__ == "__main__":
         default="w1_slave",
         help="Name of the sensor file to search for",
     )
+    parser.add_argument(
+        "--period",
+        default=60,
+        help="Period for polling in seconds",
+    )
     args = parser.parse_args()
 
     root_dir = pathlib.Path(args.root_dir)
     filename = args.sensor_filename
+    period = args.period
+
+    logger.info(f"root-dir: {root_dir}, filename: {filename}, period: {period}")
 
     logger.info(f"Searching for {filename} in {root_dir}")
     sensor_file_path = utils.search_file(root_dir, filename)[0]
