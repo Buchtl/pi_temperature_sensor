68ac5fa299cb60c6565205647569bede29893f5c
Chris <schauer@inf-schauer.de>
2025-08-14 17:59:29 +0200

    refactor

--- src/plot_temperature.py
+++ src/plot_temperature.py
@@ -4,19 +4,9 @@ from src import logging_conf
 
 logger = logging_conf.config("plot_temperature")
 
-# /sys/bus/w1/devices/10-000803cd476f
-
 
 def search_data_file(root_dir: pathlib.Path, filename: str):
     return list(root_dir.rglob(filename))
-
-
-# def search_data_file(root_dir: pathlib.Path, filename: str):
-#    results = []
-#    for dirpath, dirnames, filenames in os.walk(root_dir, followlinks=True):
-#        if filename in filenames:
-#            results.append(os.path.join(dirpath, filename))
-#    return results
 
 
 if __name__ == "__main__":
@@ -25,7 +15,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--root-dir",
-        default="/sys/bus/w1/devices",
+        default="/sys/devices",
         help="Dir where to search for sensor",
     )
     parser.add_argument(
@@ -40,5 +30,4 @@ if __name__ == "__main__":
 
     logger.info(f"Searching for {filename} in {root_dir}")
     sensor_file_path = search_data_file(root_dir, filename)[0]
-
     logger.info(f"sensor file path is: {sensor_file_path}")
