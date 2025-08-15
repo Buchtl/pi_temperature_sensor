37ca2de4ef1f4b78f244483530e91a98b5fd4001
Chris <schauer@inf-schauer.de>
2025-08-15 17:50:58 +0200

    service

---
+++ install_servcie.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo cp ./systemd/plot_temperature.servcie /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable plot_temperature.service
+sudo systemctl start plot_temperature.service
+
+
+# check
+sudo systemctl status plot_temperature.service
+sudo journalctl -u plot_temperature.service -f
