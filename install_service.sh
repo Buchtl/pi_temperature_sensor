86db143db3d1ec40ef3739df2ac0961a957d14bc
Chris <schauer@inf-schauer.de>
2025-08-15 17:53:55 +0200

    ...

--- install_service.sh
+++ install_service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cp ./systemd/plot_temperature.servcie /etc/systemd/system/
+sudo cp ./systemd/plot_temperature.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable plot_temperature.service
 sudo systemctl start plot_temperature.service
